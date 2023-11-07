f2f9a03d8a8cd0a03e33d10131f9b82c86df4430
Anyangotyra <tyrasophie@students.uonbi.ac.ke>
2023-11-07 03:47:57 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,5 +14,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))#!/usr/bin/python3
-# 2-args.py
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
